f2fa5481b6f8cb232bbd4fd18f9fd8b3a92be3ae
lucaw <luca.weisskopf@students.fhnw.ch>
2022-05-24 10:02:54 +0200

    404 and 403 pages work

---
+++ src/main/java/ch/fhnw/acrm/controller/AppErrorController.java
@@ -0,0 +1,31 @@
+package ch.fhnw.acrm.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+//Author: Luca
+@Controller
+public class AppErrorController implements ErrorController {
+
+
+    @RequestMapping("/error")
+    public String renderErrorPage(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+            switch (statusCode) {
+                case 403:
+                    return "403.html";
+                case 404:
+                    return "404.html";
+            }
+        }
+        return "login";
+    }
+
+}
